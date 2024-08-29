44019bb33a59455865d9f4e38284dafa12b3772e
dragon <dragon@dragon.dragon>
2024-08-29 12:08:58 +0530

    Initial commit

--- src/nfid-login-frontend/src/App.jsx
+++ src/nfid-login-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { NFID } from "@nfid/embed";
 import { Principal } from '@dfinity/principal';
 
+
 const App = () => {
   const [nfid, setNfid] = useState(null);
   const [delegation, setDelegation] = useState(null);
@@ -14,6 +15,11 @@ const App = () => {
           application: {
             name: "NFID Login",
             logo: "https://dev.nfid.one/static/media/id.300eb72f3335b50f5653a7d6ad5467b3.svg"
+          },
+          idleOptions: {
+            idleTimeout: 600000, // 10 minutes
+            captureScroll: true,
+            scrollDebounce: 100,
           },
         });
         setNfid(nfIDInstance);
@@ -31,14 +37,19 @@ const App = () => {
     //current time NFID call the mainnet canister if your backend on mainnet then use the env file if you are on local then use my canister id backend which is deployed over mainnet
 
     
-    // const canisterArray = ["fnrir-yiaaa-aaaak-qirwa-cai"];
-    const canisterArray = [process.env.CANISTER_ID_NFID_LOGIN_BACKEND];
+    const canisterArray = ["fnrir-yiaaa-aaaak-qirwa-cai"];
+    // const canisterArray = [process.env.CANISTER_ID_NFID_LOGIN_BACKEND];
     
     if (nfid) {
       try {
+        const identity = nfid.getIdentity();
+        console.log(identity);
+
         const delegationResult = await nfid.getDelegation({ targets: canisterArray });
         const theUserPrincipal = Principal.from(delegationResult.getPrincipal()).toText();
         console.log(theUserPrincipal);
+        const isLogin = await nfid.getDelegationType();
+        console.log(isLogin,'Delegation type');
         setDelegation(theUserPrincipal);
       } catch (error) {
         console.error("Error during NFID call:", error);
